1dcf6f2473d5d6d5e2aa43695288be198f8d4d25
JunHyeong An <sure083080@gmail.com>
2024-07-11 11:46:00 +0900

    url 변경

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -13,7 +13,7 @@ const config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://mbaas-docs.github.io',
+  url: 'https://docs.aiapp.help',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
