bd49d3119acd37fbbdea4d5ab96ef584ea4bab6d
JunHyeong An <sure083080@gmail.com>
2024-07-12 16:59:45 +0900

    1차적용

--- src/components/HomepageFeatures/index.js
+++ src/components/HomepageFeatures/index.js
@@ -8,8 +8,7 @@ const FeatureList = [
     Svg: require('@site/static/img/undraw_docusaurus_mountain.svg').default,
     description: (
       <>
-        Docusaurus was designed from the ground up to be easily installed and
-        used to get your website up and running quickly.
+        쉽게 사용하세요.
       </>
     ),
   },
@@ -18,18 +17,16 @@ const FeatureList = [
     Svg: require('@site/static/img/undraw_docusaurus_tree.svg').default,
     description: (
       <>
-        Docusaurus lets you focus on your docs, and we&apos;ll do the chores. Go
-        ahead and move your docs into the <code>docs</code> directory.
+        컨텐츠에 집중하세요.
       </>
     ),
   },
   {
-    title: 'Powered by React',
+    title: 'Powered by Flutter',
     Svg: require('@site/static/img/undraw_docusaurus_react.svg').default,
     description: (
       <>
-        Extend or customize your website layout by reusing React. Docusaurus can
-        be extended while reusing the same header and footer.
+        Flutter 기반앱 등 컨텐츠 보충
       </>
     ),
   },
